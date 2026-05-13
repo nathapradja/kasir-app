443d8b9a3aa461115d37721b86429bfea8ff6427
Nathapradja <natabintang24@gmail.com>
2026-05-13 10:32:16 +0700

    Initial project setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kasir App</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    
+    <h1>Kasir App</h1>
+
+    <script src="assets/js/script.js"></script>
+</body>
+</html>

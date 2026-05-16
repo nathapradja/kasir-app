5e1d20922b230b905472fcc93f22855db42fde70
Nathapradja <natabintang24@gmail.com>
2026-05-16 12:28:34 +0700

    add readme

---
+++ README.d
@@ -0,0 +1,35 @@
+# Kasir App
+
+Aplikasi kasir berbasis PHP Native dan PostgreSQL.
+
+## Fitur
+
+- Login Authentication
+- Dashboard Statistik
+- CRUD Barang
+- CRUD Kategori
+- Search Barang
+- Pagination
+- Upload Gambar Barang
+- Transaksi Kasir
+- Detail Transaksi
+- Cetak Struk
+- Laporan Penjualan
+- Chart Dashboard
+- Responsive Sidebar
+
+## Tech Stack
+
+- PHP Native
+- PostgreSQL
+- HTML
+- CSS
+- JavaScript
+
+## Screenshot
+
+
+
+## Author
+
+Nama Kamu
